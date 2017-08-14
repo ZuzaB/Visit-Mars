b3e667c2746521845805f21e0ced6c8b818eae6e
ZuzaB <lacznaz@wp.pl>
2017-08-14 18:50:01 +0200

    infinity loop in mars gallery

--- js/app.js
+++ js/app.js
@@ -2,10 +2,11 @@ $(function(){
 
     var nasaUrl = "https://api.nasa.gov/planetary/apod?api_key=6WpwqyBa7pKSUeUnrmYG6efhmGwNgyZQYhuU0Obr";
     var $top = $('.top');
-    var nasaGaleryUrl ="https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=2016-6-3&api_key=6WpwqyBa7pKSUeUnrmYG6efhmGwNgyZQYhuU0Obr";
+    var nasaGaleryUrl = "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=2016-6-3&api_key=6WpwqyBa7pKSUeUnrmYG6efhmGwNgyZQYhuU0Obr";
     var $sectionBottom = $('.bottom')
     var $divGallery = $sectionBottom.find('.image');
     var $button = $('button');
+    var numberImg = 0;
 
     console.log($divGallery);
 
@@ -15,19 +16,18 @@ $(function(){
     }
 
     $.ajax({
-            url: nasaUrl+"&date="+RandomDate()
+            url: nasaUrl + '&date=' + RandomDate()
         }).done(function (response) {
             imageTop(response);
         }).fail(function (error) {
-            console.log('error');
+            alert('Nie można pobrać danych z serwera.');
     });
 
     function galeryImage (marsImages) {
 
         $.each(marsImages, function(indexImg, marsImg){
-            var $divImg = $('<div class="col-lg-4 col-sm-6 col-sx-12"><img src="'+ marsImg.img_src +'" class="img-responsive"></div>');
-            $divGallery.append($divImg);
-            return (indexImg !== 5);
+              var $divImg = $('<div class="col-lg-4 col-sm-6 col-sx-12"><img src="'+ marsImg.img_src +'" class="img-responsive"></div>');
+              $divGallery.append($divImg);
         });
     }
 
@@ -40,22 +40,32 @@ $(function(){
         var $year = getRandomInt(2010, 2016);
         var $mouth = getRandomInt(1, 12);
         var $day = getRandomInt(1, 28);
-        var $date = $year+'-'+$mouth+'-'+$day
+        var $date = $year + '-' + $mouth + '-' + $day;
 
         return $date;
     }
 
-     $.ajax({
+$(window).scroll( function () {
+    var scrollTop = $(document).scrollTop();
+    var windowHeight = $(window).height();
+    var bodyHeight = $(document).height() - windowHeight;
+    var scrollPercentage = (scrollTop / bodyHeight);
+
+    if(scrollPercentage > 0.9) {
+      $.ajax({
             url: nasaGaleryUrl
         }).done(function (response) {
                 galeryImage(response.photos);
         }).fail(function (error) {
-            console.log('error');//wyświetlić info
-     });
+            alert('Nie można pobrać danych z serwera.');
+      });
+    }
+});
 
-   $button.on('click', function (){//zmienić na infinity scrolling?
-     galeryImage();
-     console.log('error');
-   });
+  //    loadImage();
+  //  $button.on('click', function (){//dodać button?
+  //    numberImg += 3;
+  //    loadImage();
+  //  });
 
 });
